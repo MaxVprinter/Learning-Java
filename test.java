130eee6fcb89b751f60bea6519fd64520ae77e19
MaxVprinter <max@vannorsdel.com>
2026-03-08 21:18:12 -0500

    first commit

---
+++ test.java
@@ -0,0 +1,25 @@
+public class test {
+    public static void main(String[] args) {
+    
+        double targetNum =3;
+
+        double num1 = 1.0;
+        double num2 = num1 + 1.0;
+        double num3 = num2 + 1.0;
+ System.out.println(num1);
+        if (num1<targetNum){
+            num1 += 1;
+            System.out.println(num2);
+
+        }if (num2<targetNum){
+            num2 += 1;
+            System.out.println(num3);
+        }
+        
+        if (num3 == targetNum){
+            System.out.println("Target number is reached script successfully executed");
+        }
+
+            
+    }
+}
